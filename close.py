d7af36ecf51d9de3c4d7b9e2a61ef6041fe30f02
Jahid Razan <45145139+jahid-razan@users.noreply.github.com>
2020-07-10 12:16:31 +0200

    Add files via upload

---
+++ close.py
@@ -0,0 +1,9 @@
+import shutil
+#shutil.rmtree('output/artists')
+#shutil.rmtree('output/songplays')
+shutil.rmtree('output/songs')
+shutil.rmtree('output/time_df')
+shutil.rmtree('output/users')
+
+
+
