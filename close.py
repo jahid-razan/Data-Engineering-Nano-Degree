e7115ef349ce64a24713dd5e6937583875cb9891
Jahid Razan <45145139+jahid-razan@users.noreply.github.com>
2020-07-10 12:17:25 +0200

    Delete close.py

--- close.py
+++
@@ -1,9 +0,0 @@
-import shutil
-#shutil.rmtree('output/artists')
-#shutil.rmtree('output/songplays')
-shutil.rmtree('output/songs')
-shutil.rmtree('output/time_df')
-shutil.rmtree('output/users')
-
-
-
